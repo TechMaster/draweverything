f4485e502afb599d92cc76b668f9eb275515f4d6
minhcuong <cuong@techmaster.vn>
2017-02-21 15:01:51 +0700

    Update Index.js

--- index.js
+++ index.js
@@ -5,3 +5,37 @@
  *
  * function drawPoint(row, col) return boolean. true draw + sign, false draw space
  */
+
+
+/***
+ *
+ * @param row {Number}
+ * @param col {Number}
+ * @return {Boolean} True vẽ + False vẽ
+ */
+function drawPoint(row, col) {
+
+}
+/***
+ *
+ * @param row
+ * @param length
+ * @param drawPoint
+ */
+function drawLine(row, length, drawPoint) {
+
+}
+function drawEverything() {
+  //TODO: Nhập dữ liệu từ người dùng
+
+  //TODO: validate dữ liệu nếu thấy bất hợp lý thì throw error hoặc thoát chương trình
+
+
+  let rows, length = initDraw();
+
+  for (let i = 0; i < rows; i++) {
+    drawLine(i, length, drawPoint)
+  }
+}
+
+drawEverything();
