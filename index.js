dc9805534e6d0f0715fdc3daeb0f04ab3aad1b36
minhcuong <cuong@techmaster.vn>
2017-02-21 23:29:34 +0700

    Draw Box

--- index.js
+++ index.js
@@ -5,9 +5,11 @@
  *
  * function drawPoint(row, col) return boolean. true draw + sign, false draw space
  */
+let rows, cols;
 
 function initDraw() {
-  return [10, 10];
+  rows = 20;
+  cols = 20;
 }
 /***
  *
@@ -15,24 +17,29 @@ function initDraw() {
  * @param col {Number}
  * @return {Boolean} True vẽ + False vẽ
  */
-function drawPoint(row, col) {
-  //return true;
-  if (row % 2 ===0) {
-    return col % 2 === 0;
+function drawAll() {
+  return true;
+}
+
+function drawStar(rowIndex, colIndex) {
+  if (rowIndex % 2 ===0) {
+    return colIndex % 2 === 0;
   } else {
-    return col % 2 !== 0;
+    return colIndex % 2 !== 0;
   }
 }
-/***
- *
- * @param row
- * @param length
- * @param drawPoint
- */
-function drawLine(row, length, drawPoint) {
+
+function drawBox(rowIndex, colIndex, rows, length) {
+  let thick = 2;
+  return (rowIndex < thick || rowIndex >= rows - thick || colIndex < thick || colIndex >= length - thick);
+
+}
+
+
+function drawLine(rowIndex, cols, drawFunction) {
   let str = "";
-  for (let k = 0; k < length; k++) {
-      str = str.concat(drawPoint(row, k) ? '+' : ' ');
+  for (let colIndex = 0; colIndex < cols; colIndex++) {
+      str = str.concat(drawFunction(rowIndex, colIndex, rows, cols) ? '+' : ' ');
   }
   return str;
 }
@@ -41,11 +48,10 @@ function drawEverything() {
 
   //TODO: validate dữ liệu nếu thấy bất hợp lý thì throw error hoặc thoát chương trình
 
-
-  let [rows, length] = initDraw();
+  initDraw();
 
   for (let i = 0; i < rows; i++) {
-    console.log(drawLine(i, length, drawPoint));
+    console.log(drawLine(i, cols, drawBox));
   }
 }
 
